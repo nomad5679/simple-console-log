7436bc680a18846fe3e8389a3c431d9f255363f0
nomad5679 <93387966+nomad5679@users.noreply.github.com>
2021-12-14 12:29:11 -0500

    Create analytics.js

---
+++ analytics.js
@@ -0,0 +1,2 @@
+console.log('Website Version: v0.0 - Logs: Add logs here')
+//This part of the script is what you set for your versions. I reccomend keeping track of even smaller versions so you can see what you have done in the past and when. If you add a log here I reccomend copy and pasting it into the index.html using the easy outline at the bottom.
